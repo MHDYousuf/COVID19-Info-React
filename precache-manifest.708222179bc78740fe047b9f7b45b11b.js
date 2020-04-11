cb692c559bf97f565dca4917c0ce10a005b42e9e
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2020-04-11 20:48:31 +0530

    Updates

---
+++ precache-manifest.708222179bc78740fe047b9f7b45b11b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b057c09d68c329f2ddcd3553dc21675",
+    "url": "/COVID19-Info-React/index.html"
+  },
+  {
+    "revision": "d8b5ad0ce7236457c512",
+    "url": "/COVID19-Info-React/static/css/main.446b421f.chunk.css"
+  },
+  {
+    "revision": "b5fd21c631d04c107a28",
+    "url": "/COVID19-Info-React/static/js/3.a5f1c685.chunk.js"
+  },
+  {
+    "revision": "05ca525fac8eca843a636d82adf584ed",
+    "url": "/COVID19-Info-React/static/js/3.a5f1c685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45908eb0a7069975a4ef",
+    "url": "/COVID19-Info-React/static/js/4.71be96a3.chunk.js"
+  },
+  {
+    "revision": "b7059f22b1e7f387cd18",
+    "url": "/COVID19-Info-React/static/js/5.b335d7a0.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/COVID19-Info-React/static/js/5.b335d7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ae9e622f56d603ef6b",
+    "url": "/COVID19-Info-React/static/js/6.4fc0e811.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/COVID19-Info-React/static/js/6.4fc0e811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b5ad0ce7236457c512",
+    "url": "/COVID19-Info-React/static/js/main.c54faa08.chunk.js"
+  },
+  {
+    "revision": "f62f804952e42679f27b",
+    "url": "/COVID19-Info-React/static/js/runtime-main.8b44d39e.js"
+  },
+  {
+    "revision": "0796c25fa018f1067cad",
+    "url": "/COVID19-Info-React/static/js/xlsx.126ed539.chunk.js"
+  }
+]);
---
+++ static/js/xlsx.126ed539.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcorona-app"]=this["webpackJsonpcorona-app"]||[]).push([[2],{328:function(n,o){},417:function(n,o){},418:function(n,o){}}]);
+//# sourceMappingURL=xlsx.126ed539.chunk.js.map
